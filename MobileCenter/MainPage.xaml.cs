99ebaeb0d7aeb03e087a7f8dca02a701f3aaffe6
JoeM-RP <jmeyer@rightpoint.com>
2017-06-22 23:00:32 -0500

    iOS + test features

--- MobileCenter/MainPage.xaml.cs
+++ MobileCenter/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace Mobile_Center
         {
             InitializeComponent();
         }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
